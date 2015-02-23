e55f4e071b377f4af97f4aca97401f64ebfd4725
Tristan Wagar <trwa7487@colorado.edu>
2015-02-22 20:34:53 -0700

    build

--- js/my/repos.js
+++ js/my/repos.js
@@ -1,7 +1,27 @@
-function myRepos(){
+function myRepos(full_name){
 
-    // display my repos in #list
-    // e.g., https://api.github.com/users/doubleshow/repos
+    console.log('listing repos')    
+    
+    $.get("https://api.github.com/users/twagar95/repos", github, function(data) {
 
-    alert('todo')
+        // Q: What is the parameter 'github'? Where was it defined? What's the purpose? 
+
+        // Q: Why is JSON.parse no longer necessary?
+        var repos = data
+        
+        // Q. Why are these templates files stored in a separate folder inside contents/?
+        $.get("/git-jquery/templates/repoList.jade", function(template) {
+
+            // render the template
+            var html = jade.render(template, {items: repos})            
+
+            // assign the rendered html to the dom element whose id is #list
+            $("#list").html(html)
+
+            // load the first repo to view
+            myRepo(repos[0].full_name)
+
+        })
+
+    })
 }
