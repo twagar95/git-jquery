02bb65b55d5da1884fd89125086aca1c40639929
Tristan Wagar <trwa7487@colorado.edu>
2015-02-22 20:34:53 -0700

    build

--- build/js/my/events.js
+++ build/js/my/events.js
@@ -3,5 +3,29 @@ function myEvents(){
     // display a list of events belonging to me in #list
     // e.g., https://api.github.com/users/doubleshow/events
 
-    alert('todo')
+    console.log('listing events')    
+    
+    $.get("https://api.github.com/users/twagar95/events", github, function(data) {
+
+        // Q: What is the parameter 'github'? Where was it defined? What's the purpose? 
+
+        // Q: Why is JSON.parse no longer necessary?
+        var events = data
+        var user = events[0].actor.login
+        
+        // Q. Why are these templates files stored in a separate folder inside contents/?
+        $.get("/git-jquery/templates/eventList.jade", function(template) {
+
+            // render the template
+            var html = jade.render(template, {items: events, user: user})            
+
+            // assign the rendered html to the dom element whose id is #list
+            $("#list").html(html)
+
+            
+            $("#details").html("")
+
+        })
+
+    })
 }
--- build/js/my/repo.js
+++ build/js/my/repo.js
@@ -1,7 +1,22 @@
 function myRepo(full_name){
 
-    // display a particular repo of mine in #details
-    // e.g., https://api.github.com/repos/doubleshow/up
+    console.log('viewing repo: ', full_name)
 
-    alert('todo')
+    // e.g., https://api.github.com/repos/ucdd2-sp15/nn-express
+
+    $.get("https://api.github.com/repos/" + full_name, github, function(data) {
+        
+        var repo = data
+        // console.log(repo)
+
+        $.get("/git-jquery/templates/repoView.jade", function(template) {
+
+            var html = jade.render(template, {item: repo})
+            
+            $("#details").html(html)
+
+        })
+
+    })
+
 }
